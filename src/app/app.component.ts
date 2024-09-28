a36856b4cd322044bf7424a80b4bd3b6071cd402
satrioo <satrioari43@gmail.com>
2024-09-28 12:10:04 +0700

    update header & footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'news';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  async ngAfterViewInit(): Promise<void> {
+    if (isPlatformBrowser(this.platformId)) {
+      // Dynamically import Flowbite only when the platform is the browser
+      const { initFlowbite } = await import('flowbite');
+      initFlowbite();
+    }
+  }
 }
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { initFlowbite } from 'flowbite';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css',
+// })
+// export class AppComponent implements OnInit {
+//   title = 'belajar_angular';
+
+//   ngOnInit(): void {
+//     initFlowbite();
+//   }
+// }
